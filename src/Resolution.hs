5c24a4c5c11025cc0483475493edc6719f0fae53
marvinpolster <marvin.polster@gmail.com>
2020-10-04 18:21:29 +0200

    bugfixes + improvements
    - add .gitignore
    
    Resolution.hs
    - remove contains_complement (superfluous)
    - added error handling in resX
    - sort clauses in resX before resolving instead of sorting in resN
    - improved/simplified patterns in resXBE
    
    test.hs
    - added tests for error handling and resX

--- src/Resolution.hs
+++ src/Resolution.hs
@@ -4,7 +4,7 @@ module Resolution (Literal (..), resX, resN) where
 import Data.List (union, sort)
 
 -- Fundamental Data Structures and type aliases
-data Literal = Literal {neg :: Bool, name :: String}
+data Literal = Literal {sig :: Bool, name :: String}
 type Clause = [Literal]
 type ClauseSet = [Clause]
 
@@ -17,17 +17,13 @@ instance Eq Literal where
 instance Ord Literal where
     compare (Literal a b) (Literal x y) = if compare b y == EQ then compare a x else compare b y
 
--- Returns the Complement of a Literal
+-- Returns the complementary literal
 complement :: Literal -> Literal
-complement x = Literal (not $ neg x) (name x)
-
--- Returns True if complement of a literal is in the clause, else False
-contains_complement :: Literal -> Clause -> Bool
-contains_complement l c = (Literal (not $ neg l) (name l)) `elem`c
+complement x = Literal (not $ sig x) (name x)
 
 -- Returns the sorted resolvent of two clauses
 resolvent :: Literal -> Clause -> Clause -> Clause
-resolvent l x y = sort ((filter(\z -> z /= l) x) `union` (filter(\z -> z /= complement l) y))
+resolvent l x y = sort((filter(\z -> z /= l) x) `union` (filter(\z -> z /= complement l) y))
 
 -- Initialize resolvation for two Clauses
 resolve :: Clause -> Clause -> ClauseSet
@@ -37,28 +33,30 @@ resolve x y = resolveBE 0 x y []
 resolveBE :: Int -> Clause -> Clause -> ClauseSet -> ClauseSet
 resolveBE n xs y acc 
     | n == length xs = acc
-    | contains_complement (xs !! n) y && not ((resolvent (xs !! n) xs y) `elem` acc) = resolveBE (n+1) xs y ((resolvent (xs !! n) xs y):acc)
+    | (complement (xs !! n)) `elem` y && not ((resolvent (xs !! n) xs y) `elem` acc) = resolveBE (n+1) xs y ((resolvent (xs !! n) xs y):acc)
     | otherwise = resolveBE (n+1) xs y acc
 
 -- Initialize the resolvation
 resN :: ClauseSet -> ClauseSet
-resN x = resNBE 0 (head sort_x) (tail sort_x) sort_x where sort_x = (map (\l -> sort l) x)
+resN x = resNBE 0 (head x) (tail x) x
 
--- Make all resolvents of Res(n-1)
+-- Make all resolvents for Res(n)
 resNBE :: Int -> Clause -> ClauseSet -> ClauseSet -> ClauseSet
 resNBE n x y acc 
     | null y = acc
     | n == length y = resNBE 0 (head y) (tail y) acc
     | otherwise = resNBE (n+1) x y (acc `union` (resolve x (y !! n)))
 
--- Init calculating Res(*)
+-- Init calculating Res(*), error handling
 resX :: ClauseSet -> (String, Int, ClauseSet)
-resX x = if a == True then ("Satisfiable!", b, c) else ("Not Satisfiable!", b, c) where (a, b, c) = resXBE x 0 []
+resX x = case resXBE (map (\c -> sort c) x) 0 [] of
+    Just (a, b, c) -> if a == True then ("Satisfiable!", b, c) else ("Not Satisfiable!", b, c)
+    Nothing -> ("Error!", 0, [])
 
--- Calculate Res(n) until [] € Res(n) or Res(n) = Res(n-1)
-resXBE :: ClauseSet -> Int -> ClauseSet -> (Bool, Int, ClauseSet)
+-- Calculate Res(n) until [] \in Res(n) or Res(n) = Res(n-1)
+resXBE :: ClauseSet -> Int -> ClauseSet -> Maybe (Bool, Int, ClauseSet)
 resXBE x n acc 
-    | [] `elem` acc = (False, n, acc)
-    | acc `union`(resN (x `union` acc)) == acc = (True, n, acc `union`(resN (x `union` acc)))
-    | otherwise = resXBE (acc `union` x) (n+1) (acc `union` resn_x)
-    where resn_x = resN x
+    | null x = Nothing
+    | [] `elem` x = Just (False, n, x)
+    | x == acc = Just (True, (n-1), x)
+    | otherwise = resXBE (acc `union` (resN x)) (n+1) (acc `union` x)
